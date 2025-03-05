2d6f8f1bbc9ef31e97baa670e863593eb612e2f3
Alexander Ilves <ailves2009@gmail.com>
2025-03-05 15:33:04 +0100

    change owner

--- index.js
+++ index.js
@@ -4,12 +4,6 @@
 //-----------------------------------------------------------------------------
 
 exports.helloWorld = (req, res) => {
-<<<<<<< HEAD
-  const message="<font color='blue'>СloudFunction of Denis Astahov!</font><br><b>App Version 1.3</b>";
-||||||| parent of 7d0951d (update project)
-  const message="<font color='blue'>СloudFunction of Denis Astahov!</font><br><b>App Version 1.1</b>";
-=======
   const message = "<font color='blue'>СloudFunction of Aleksandr Ilves!</font><br><b>App Version 1.1</b>";
->>>>>>> 7d0951d (update project)
   res.status(200).send(message);
 };
